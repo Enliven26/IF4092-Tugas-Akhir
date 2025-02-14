9ba3dad97af402031dfa641296073bf3c3fb1499
johanes lee <johaneslee26@gmail.com>
2025-02-14 21:44:49 +0700

    feat: add pip install setup

--- src/autocommit/core/parsers/language/java.py
+++ src/autocommit/core/parsers/language/java.py
@@ -12,24 +12,18 @@ class JavaCodeParser(ICodeParser):
 
     def __run_java_parser(self, java_source_code: str, line_ranges: list[range]) -> str:
         temp_file_path = ""
-        
+
         with tempfile.NamedTemporaryFile(delete=False, suffix=".txt") as temp_file:
             temp_file_path = temp_file.name
             temp_file.write(java_source_code.encode("utf-8"))
 
-        target_dir = os.path.join("..", "java", "javaparser", "target")
-
+        jar_path = os.path.join("jars", "javaparser.jar")
         line_ranges_json = json.dumps([[r.start, r.stop] for r in line_ranges])
-        classes_dir = os.path.join(target_dir, "classes")
-        lib_dir = os.path.join(target_dir, "lib", "*")
-        classpath = os.pathsep.join([classes_dir, lib_dir])
-        main_class = "tugasakhir.javaparser.JavaParser"
 
         command = [
             "java",
-            "-cp",
-            classpath,
-            main_class,
+            "-jar",
+            jar_path,
             temp_file_path,
             line_ranges_json,
         ]
